36871b2b190b8fcb43e3c43f5baa329f3370cc7f
gabrielgamboa <gabrielgamboav19@gmail.com>
2021-09-13 22:31:29 -0600

    Minor changes

--- First Draft/readme.md
+++
@@ -1,6 +0,0 @@
-Format for the messages between backend and frontend
-	Every response will have a success: true or success: false  in the root of the json file
-	always in lowercase.
-	If success: false happens there will be an error: error message where error message
-	will be in a format defined for each instance so that React can handle it appropriately.
-	
---
+++ Share (Uploaded version)/server/static/tailwind.config.js
@@ -0,0 +1,48 @@
+module.exports = {
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+
+    screens:{
+      'lit':'400px',
+
+      'litx':'520px',
+
+      'sm': '640px',
+
+      'md': '768px',
+
+      'lg': '1024px',
+
+      'xl': '1280px',
+
+      '2xl': '1536px',
+
+    },
+
+    fontFamily:{
+      "oxy" : ['Oxygen', 'sans-serif'],
+      "lato":['Lato', 'sans-serif']
+    },
+    colors: {
+      // Build your palette here
+      "top-nav" : "#FF7400",
+      "background": "#EBD8CA",
+      "white" : "#F8FFFC",
+      "nav-link" : "#AB560F",
+      "nav-link-text": "#451C00",
+      "bor-comm" : '#9E816C',
+      'light-blue':'#0DAAF9'
+    },
+    extend: {
+        outline: {
+          editor : '2px solid #BDEED7',
+          "top-nav" : '2px solid #BDEED7',
+          "nav-link" : '1px solid #BDEED7',
+        }
+      }
+  },
+  variants: {
+
+  },
+  plugins: [],
+}
